05726a266ed872a9e98a8d7612a3b855c9a2a985
Пользователь <tu7c1ne72@code.edu.az>
2026-07-22 01:57:44 +0400

    bugs fixed

--- src/components/Home/HomeGallerySection/index.jsx
+++ src/components/Home/HomeGallerySection/index.jsx
@@ -1,82 +1,24 @@
-
+import image1 from "../assets/images1.jpg";
+import image2 from "../assets/images2.jpg";
+import image3 from "../assets/images3.jpg";
 
 const HomeGallerySection = () => {
-  const galleryData = [
-    {
-      id: 1,
-      image: "/src/assets/images1.jpg",
-      title: "Web Development",
-    },
-    {
-      id: 2,
-      image: "/src/assets/images2.jpg",
-      title: "Software Engineering",
-    },
-    {
-      id: 3,
-      image: "/src/assets/images3.jpg",
-      title: "UI / UX Design",
-    },
-       {
-      id: 2,
-      image: "/src/assets/images2.jpg",
-      title: "Software Engineering",
-    },
-    {
-      id: 3,
-      image: "/src/assets/images3.jpg",
-      title: "UI / UX Design",
-    },
-       {
-      id: 2,
-      image: "/src/assets/images2.jpg",
-      title: "Software Engineering",
-    },
-    {
-      id: 3,
-      image: "/src/assets/images3.jpg",
-      title: "UI / UX Design",
-    },
-       {
-      id: 2,
-      image: "/src/assets/images2.jpg",
-      title: "Software Engineering",
-    },
-    {
-      id: 3,
-      image: "/src/assets/images3.jpg",
-      title: "UI / UX Design",
-    },
-       {
-      id: 2,
-      image: "/src/assets/images2.jpg",
-      title: "Software Engineering",
-    },
-    {
-      id: 3,
-      image: "/src/assets/images3.jpg",
-      title: "UI / UX Design",
-    },
-       {
-      id: 2,
-      image: "/src/assets/images2.jpg",
-      title: "Software Engineering",
-    },
-    {
-      id: 3,
-      image: "/src/assets/images3.jpg",
-      title: "UI / UX Design",
-    },
-       {
-      id: 2,
-      image: "/src/assets/images2.jpg",
-      title: "Software Engineering",
-    },
-    {
-      id: 3,
-      image: "/src/assets/images3.jpg",
-      title: "UI / UX Design",
-    },
+ const galleryData = [
+    { id: 1, image: image1, title: "Web Development" },
+    { id: 2, image: image2, title: "Software Engineering" },
+    { id: 3, image: image3, title: "UI / UX Design" },
+    { id: 4, image: image2, title: "Software Engineering" },
+    { id: 5, image: image3, title: "UI / UX Design" },
+    { id: 6, image: image2, title: "Software Engineering" },
+    { id: 7, image: image3, title: "UI / UX Design" },
+    { id: 8, image: image2, title: "Software Engineering" },
+    { id: 9, image: image3, title: "UI / UX Design" },
+    { id: 10, image: image2, title: "Software Engineering" },
+    { id: 11, image: image3, title: "UI / UX Design" },
+    { id: 12, image: image2, title: "Software Engineering" },
+    { id: 13, image: image3, title: "UI / UX Design" },
+    { id: 14, image: image2, title: "Software Engineering" },
+    { id: 15, image: image3, title: "UI / UX Design" },
   ];
   return (
     <section
