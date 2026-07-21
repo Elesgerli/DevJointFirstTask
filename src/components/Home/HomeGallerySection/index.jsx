1c37b17e61c2d75af75bc4de81bd3b2a964f2aa5
Пользователь <tu7c1ne72@code.edu.az>
2026-07-22 01:09:56 +0400

    added gallery section and his responsivly functions

--- src/components/Home/HomeGallerySection/index.jsx
+++ src/components/Home/HomeGallerySection/index.jsx
@@ -1,11 +1,129 @@
-import React from 'react'
+
 
 const HomeGallerySection = () => {
+  const galleryData = [
+    {
+      id: 1,
+      image: "/src/assets/images1.jpg",
+      title: "Web Development",
+    },
+    {
+      id: 2,
+      image: "/src/assets/images2.jpg",
+      title: "Software Engineering",
+    },
+    {
+      id: 3,
+      image: "/src/assets/images3.jpg",
+      title: "UI / UX Design",
+    },
+       {
+      id: 2,
+      image: "/src/assets/images2.jpg",
+      title: "Software Engineering",
+    },
+    {
+      id: 3,
+      image: "/src/assets/images3.jpg",
+      title: "UI / UX Design",
+    },
+       {
+      id: 2,
+      image: "/src/assets/images2.jpg",
+      title: "Software Engineering",
+    },
+    {
+      id: 3,
+      image: "/src/assets/images3.jpg",
+      title: "UI / UX Design",
+    },
+       {
+      id: 2,
+      image: "/src/assets/images2.jpg",
+      title: "Software Engineering",
+    },
+    {
+      id: 3,
+      image: "/src/assets/images3.jpg",
+      title: "UI / UX Design",
+    },
+       {
+      id: 2,
+      image: "/src/assets/images2.jpg",
+      title: "Software Engineering",
+    },
+    {
+      id: 3,
+      image: "/src/assets/images3.jpg",
+      title: "UI / UX Design",
+    },
+       {
+      id: 2,
+      image: "/src/assets/images2.jpg",
+      title: "Software Engineering",
+    },
+    {
+      id: 3,
+      image: "/src/assets/images3.jpg",
+      title: "UI / UX Design",
+    },
+       {
+      id: 2,
+      image: "/src/assets/images2.jpg",
+      title: "Software Engineering",
+    },
+    {
+      id: 3,
+      image: "/src/assets/images3.jpg",
+      title: "UI / UX Design",
+    },
+  ];
   return (
-    <section id='gallery'>
-      
-    </section>
-  )
-}
+    <section
+      id="gallery"
+      className="homeGallery"
+      aria-labelledby="galleryTitle"
+    >
+      <div className="container">
 
-export default HomeGallerySection
+        <h2
+          id="galleryTitle"
+          className="homeGalleryTitle"
+        >
+          Layihələrimiz (Qalereya)
+        </h2>
+
+        <div className="homeGalleryWrapper">
+
+          {galleryData.map((item) => (
+
+            <article
+              className="homeGalleryCard"
+              key={item.id}
+            >
+
+              <img
+                src={item.image}
+                alt={item.title}
+              />
+
+              <div className="homeGalleryOverlay">
+
+                <h3>
+                  {item.title}
+                </h3>
+
+              </div>
+
+            </article>
+
+          ))}
+
+        </div>
+
+      </div>
+    </section>
+  );
+};
+
+export default HomeGallerySection;
